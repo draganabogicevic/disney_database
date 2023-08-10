ac2591390cc5dbc797ea3e7408a4f2e2d0e0c1ce
MERA\dragabog <dragana.bogicevic@orioninc.com>
2023-08-10 10:51:59 +0200

    changes in endpoint url

--- src/service/api.js
+++ src/service/api.js
@@ -10,19 +10,17 @@ const Api = axios.create({
 // };
 
 export const fetchCharactersBySearch = async (str) => {
-  const result = await Api.get(
-    `https://api.disneyapi.dev/character?name=${str}`
-  );
+  const result = await Api.get(`/character?name=${str}`);
   return result.data.data;
 };
 
 export const fetchAllCharactersByPages = async (num) => {
-  const result = Api.get(`/characters?page=${num}`);
+  const result = Api.get(`/character?page=${num}`);
   return (await result).data.data;
 };
 
 export const fetchCharacterById = async (id) => {
-  const result = Api.get(`/characters/${id}`);
+  const result = Api.get(`/character/${id}`);
   console.log(result);
   return (await result).data;
 };
